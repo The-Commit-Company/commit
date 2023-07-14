85c522d9b5a7dbba4d7bea1e4aeb545235d956c2
Nikhil Kothari <nikhil.kothari@thecommit.company>
2023-07-14 20:07:31 +0530

    fix: base react app setup

--- dashboard/src/pages/dashboard/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { InviteMemberDialog } from "@/components/features/invite-members/InviteMemberDialog"
-
-export const Dashboard = () => {
-    return (
-        <div>
-            <InviteMemberDialog />
-        </div>
-    )
-}
