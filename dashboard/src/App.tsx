83391b15686afc60f9a626fd7b66ba08b68df3e2
Sumit Jain <jainmit23@gmail.com>
2023-07-15 20:50:39 +0530

    route and package

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,24 +1,25 @@
 import { FrappeProvider } from 'frappe-react-sdk'
 import { Graph } from './pages/features/erd/Graph'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-      <FrappeProvider socketPort={import.meta.env.VITE_SOCKET_PORT ?? undefined}>
-        <BrowserRouter>
-          <UserProvider>
-            <Routes>
-              {/** Public Routes */}
-              <Route path="/sign-up" element={<SignUp />} />
+    <FrappeProvider socketPort={import.meta.env.VITE_SOCKET_PORT ?? undefined}>
+      <BrowserRouter>
+        {/* <UserProvider> */}
+        <Routes>
+          {/** Public Routes */}
+          {/* <Route path="/sign-up" element={<SignUp />} /> */}
 
-              {/** Private Routes */}
-              <Route path="/" element={<ProtectedRoute />} />
-              <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-              <Route path="/graph" element={<Graph />} />
-            </Routes>
-          </UserProvider>
-        </BrowserRouter>
-      </FrappeProvider>
+          {/** Private Routes */}
+          {/* <Route path="/" element={<ProtectedRoute />} /> */}
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/graph" element={<Graph />} />
+        </Routes>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </FrappeProvider>
   )
 }
 
