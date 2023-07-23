1b0058a00b61664fa5a1f152f2fb6ef5333c7e9e
Sumit Jain <jainmit23@gmail.com>
2023-07-24 00:04:58 +0530

    feat:utils function

--- dashboard/src/lib/utils.ts
+++ dashboard/src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import moment from "moment-timezone"
+
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const convertStringDateToMoment = (date: string) => {
+  if (date) {
+    return moment(date, 'YYYY/MM/DD')
+  }
+  return null
 }
