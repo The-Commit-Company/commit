c45b1dd8082333319b8de3e6fe733e4523ea7db2
Sumit Jain <jainmit23@gmail.com>
2023-07-24 00:05:22 +0530

    feat:table type and node

--- dashboard/src/pages/features/erd/Graph.tsx
+++ dashboard/src/pages/features/erd/Graph.tsx
@@ -5,9 +5,7 @@ import ReactFlow, {
     Background,
     Controls,
     Edge,
-    Handle,
     Node,
-    NodeProps,
     Position,
     ReactFlowProvider,
     useReactFlow,
@@ -24,11 +22,12 @@ import 'reactflow/dist/style.css'
 import { PostgresTable, PostgresRelationship, TableNodeData } from '@/types/Table'
 import { CgMaximizeAlt } from 'react-icons/cg'
 import { TbArrowsMinimize } from 'react-icons/tb'
+import { TableNode } from './TableNode'
 // import { set } from 'lodash'
 
 // ReactFlow is scaling everything by the factor of 2
-const NODE_WIDTH = 320
-const NODE_ROW_HEIGHT = 40
+export const NODE_WIDTH = 320
+export const NODE_ROW_HEIGHT = 40
 
 export const Graph = () => {
     return (
@@ -160,50 +159,6 @@ const getLayoutedElements = (nodes: Node<TableNodeData>[], edges: Edge[]) => {
 }
 
 
-function TableNode({ data, targetPosition, sourcePosition }: NodeProps<TableNodeData>) {
-    // Important styles is a nasty hack to use Handles (required for edges calculations), but do not show them in the UI.
-    // ref: https://github.com/wbkd/react-flow/discussions/2698
-    const hiddenNodeConnector = '!h-px !w-px !min-w-0 !min-h-0 !cursor-grab !border-0 !opacity-0'
-
-    return (
-        <div className={`w-[${NODE_WIDTH / 2}px ] rounded-lg overflow-hidden bg-white  shadow-sm`}>
-            <header className="text-[0.5rem] leading-5 px-2 font-bold text-center bg-blue-500 text-white">
-                {data.name}
-            </header>
-
-            {data.columns.map((column) => (
-                <div
-                    className="text-[8px] leading-5 relative flex justify-between odd:bg-scale-300 even:bg-scale-400 border-slate-100 border"
-                    key={column.id}
-                >
-                    <span
-                        className={`${column.id === "name" ? 'border-l-2 border-l-blue-500 pl-[6px] pr-2' : 'px-2'
-                            } text-ellipsis overflow-hidden whitespace-nowrap`}
-                    >
-                        {column.name}
-                    </span>
-                    <span className="px-2">{column.format}</span>
-                    {targetPosition && (
-                        <Handle
-                            type="target"
-                            id={column.id}
-                            position={targetPosition}
-                            className={`${hiddenNodeConnector} !left-0`}
-                        />
-                    )}
-                    {sourcePosition && (
-                        <Handle
-                            type="source"
-                            id={column.id}
-                            position={sourcePosition}
-                            className={`${hiddenNodeConnector} !right-0`}
-                        />
-                    )}
-                </div>
-            ))}
-        </div>
-    )
-}
 
 const TablesGraph: FC<{ tables: PostgresTable[], relationships: PostgresRelationship[] }> = ({ tables, relationships }) => {
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
