4bacf6f3657234fb6760273e083ae067dc214a9e
Yash Jane <yashjane99@gmail.com>
2023-08-11 13:03:06 +0530

    chore: fix config

--- dashboard/proxyOptions.js
+++ dashboard/proxyOptions.js
@@ -1,11 +1,16 @@
-const common_site_config = require('../../../sites/common_site_config.json');
-const { webserver_port } = common_site_config;
+let webserver_port = "8001";
+try {
+	const common_site_config = require('../../../sites/common_site_config.json');
+	webserver_port = common_site_config.webserver_port;
+} catch {
+
+}
 
 export default {
-	'^/(app|api|assets|files)': {
+	'^/(app|api|assets|files|private)': {
 		target: `http://localhost:${webserver_port}`,
 		ws: true,
-		router: function(req) {
+		router: function (req) {
 			const site_name = req.headers.host.split(':')[0];
 			return `http://${site_name}:${webserver_port}`;
 		}
