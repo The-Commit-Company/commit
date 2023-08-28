2b30fea05ec029a69f5a8b83f828a299c364227c
Sumit Jain <jainmit23@gmail.com>
2023-08-28 13:17:30 +0530

    feat:hover removed from table node

--- dashboard/src/pages/features/erd/TableNode.tsx
+++ dashboard/src/pages/features/erd/TableNode.tsx
@@ -1,8 +1,6 @@
 import { PostgresTable } from "@/types/Table";
 import { NodeProps, Handle } from "reactflow";
 import { NODE_WIDTH } from "./Graph";
-import { HoverCard, HoverCardTrigger } from "@radix-ui/react-hover-card";
-import { TableHoverCard } from "./TableHoverCard";
 
 export const TableNode = ({ data, targetPosition, sourcePosition }: NodeProps<PostgresTable>) => {
     // Important styles is a nasty hack to use Handles (required for edges calculations), but do not show them in the UI.
@@ -17,39 +15,34 @@ export const TableNode = ({ data, targetPosition, sourcePosition }: NodeProps<Po
                 </header>
 
                 {data.columns.map((column) => (
-                    <HoverCard key={column.id}>
-                        <HoverCardTrigger asChild>
-                            <div
-                                className="text-[8px] leading-5 relative flex justify-between odd:bg-scale-300 even:bg-scale-400 border-slate-100 border"
-                                key={column.id}
-                            >
-                                <span
-                                    className={`${column.id === "name" ? 'border-l-2 border-l-blue-500 pl-[6px] pr-2' : 'px-2'
-                                        } text-ellipsis overflow-hidden whitespace-nowrap`}
-                                >
-                                    {column.name}
-                                </span>
-                                <span className="px-2">{column.format}</span>
-                                {targetPosition && (
-                                    <Handle
-                                        type="target"
-                                        id={column.id}
-                                        position={targetPosition}
-                                        className={`${hiddenNodeConnector} !left-0`}
-                                    />
-                                )}
-                                {sourcePosition && (
-                                    <Handle
-                                        type="source"
-                                        id={column.id}
-                                        position={sourcePosition}
-                                        className={`${hiddenNodeConnector} !right-0`}
-                                    />
-                                )}
-                            </div>
-                        </HoverCardTrigger>
-                        <TableHoverCard column={column} />
-                    </HoverCard>
+                    <div
+                        className="text-[8px] leading-5 relative flex justify-between odd:bg-scale-300 even:bg-scale-400 border-slate-100 border"
+                        key={column.id}
+                    >
+                        <span
+                            className={`${column.id === "name" ? 'border-l-2 border-l-blue-500 pl-[6px] pr-2' : 'px-2'
+                                } text-ellipsis overflow-hidden whitespace-nowrap`}
+                        >
+                            {column.name}
+                        </span>
+                        <span className="px-2">{column.format}</span>
+                        {targetPosition && (
+                            <Handle
+                                type="target"
+                                id={column.id}
+                                position={targetPosition}
+                                className={`${hiddenNodeConnector} !left-0`}
+                            />
+                        )}
+                        {sourcePosition && (
+                            <Handle
+                                type="source"
+                                id={column.id}
+                                position={sourcePosition}
+                                className={`${hiddenNodeConnector} !right-0`}
+                            />
+                        )}
+                    </div>
                 ))}
             </div>
         </>
