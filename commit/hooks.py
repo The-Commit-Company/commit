5e7160bd55e4d4f6b85d3925cf9109627fb7ec19
Nikhil Kothari <nikhil.kothari@thecommit.company>
2023-09-07 17:58:44 +0530

    fix: build error

--- commit/hooks.py
+++ commit/hooks.py
@@ -4,7 +4,7 @@ app_name = "commit"
 app_title = "commit"
 app_publisher = "The Commit Company"
 app_description = "The Commit Company"
-app_email = "yash.jane@thecommit.company"
+app_email = "support@thecommit.company"
 app_license = "MIT"
 
 # Includes in <head>
