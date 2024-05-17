b8e26c3e53dc08ab6bb8a5c6ddc96e08c65ff7f7
Sumit Jain <jainmit23@gmail.com>
2024-05-17 14:08:26 +0530

    feat:boot added

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from './components/ui/toaster.tsx'
+import './utils/namespace'
+
+
+if (import.meta.env.DEV) {
+  fetch('/api/method/commit.www.dashboard.get_context_for_dev', {
+    method: 'POST',
+  })
+    .then(response => response.json())
+    .then((values) => {
+      const v = JSON.parse(values.message)
+      // @ts-expect-error
+      if (!window.frappe) window.frappe = {};
+      //@ts-ignore
+      frappe.boot = v
+      //@ts-ignore
+      frappe._messages = frappe.boot["__messages"];
+      //@ts-ignore
+      frappe.model.sync(frappe.boot.docs);
+
+    })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
