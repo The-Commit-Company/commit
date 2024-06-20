9db54854b88988b034c3e8811fdc459b687793f8
Pardeshi-Aditya <mailadityapardeshi@gmail.com>
2024-06-20 23:59:41 +0530

    feat : added project cards and menu button

--- dashboard/src/components/features/projects/ProjectCard2.tsx
+++ dashboard/src/components/features/projects/ProjectCard2.tsx
@@ -17,7 +17,8 @@ import { isSystemManager } from "@/utils/roles";
 import ManageBranchModal from "./Branch/ManageBranchModal";
 import { Dialog } from "@/components/ui/dialog";
 import { RxDragHandleDots1 } from "react-icons/rx";
-
+import { AiOutlineMenu } from "react-icons/ai";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubTrigger, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export interface ProjectCardProps {
   project: ProjectWithBranch
@@ -37,54 +38,69 @@ export function ProjectCard2({ project, mutate }: ProjectCardProps) {
 
   const [openManageModal, setOpenManageModal] = useState(false)
 
+  const handleMenu = () => {
+
+  }
+
   return (
-    <Card className="w-[300px]">
+    <Card className="w-[300px] h-[400px]">
       <CardHeader>
-        <div className="flex gap-2">
-          <Avatar className="h-11 w-11 rounded-md border border-gray-100">
-            <AvatarImage src={project.image} />
-            <AvatarFallback className="rounded-md">
-              {appNameInitials}
-            </AvatarFallback>
-          </Avatar>
-          <div>
-            <CardTitle>{project.display_name}</CardTitle>
-            <CardDescription>
-              {project.description}
-            </CardDescription>
+        <div className="flex gap-3 justify-between items-start">
+          <div className="flex gap-3">
+            <Avatar className="h-11 w-11 rounded-md border border-gray-100">
+              <AvatarImage src={project.image} />
+              <AvatarFallback className="rounded-md">
+                {appNameInitials}
+              </AvatarFallback>
+            </Avatar>
+            <div>
+              <CardTitle>{project.display_name}</CardTitle>
+              <CardDescription>
+                {project.description}
+              </CardDescription>
+            </div>
           </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline"><AiOutlineMenu /></Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              {isCreateAccess &&
+                <DropdownMenuItem onClick={() => { setOpenManageModal(true) }}>
+                  {project.branches.length > 0 &&
+                    <>
+                      <RxDragHandleDots1 className="h-4 w-4 mr-1" />
+                      <span> Manage Branches</span>
+                    </>
+                  }
+                </DropdownMenuItem>
+              }
+              <DropdownMenuItem>
+                <AlertDialog>
+                  {isCreateAccess && <AlertDialogTrigger asChild>
+                    <Button size='icon' variant='outline' className="h-8 w-8">
+                      <AiOutlineDelete />
+                    </Button>
+                  </AlertDialogTrigger>}
+                  <DeleteProjectModal project={project} mutate={mutate} />
+                </AlertDialog>
+              </DropdownMenuItem>
+
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </CardHeader>
-      <CardFooter className="flex justify-between">
-        {isCreateAccess &&
-          <>
-            {project.branches.length > 0 ?
-              <Button
-                variant="ghost"
-                className="w-full h-8 font-normal text-sm left-0 flex justify-start pl-1"
-                onClick={() => {
-                  setOpenManageModal(true)
-                }}>
-                <RxDragHandleDots1 className="h-4 w-4 mr-1" />
-                Manage Branches
-              </Button> :
-              <div></div>}
-          </>
-        }
-        <Dialog open={openManageModal} onOpenChange={setOpenManageModal}>
-          <ManageBranchModal branches={project.branches} mutate={mutate} setOpenManageModal={setOpenManageModal} />
-        </Dialog>
-        <AlertDialog>
-          {isCreateAccess && <AlertDialogTrigger asChild>
-            <Button size='icon' variant='outline' className="h-8 w-8">
-              <AiOutlineDelete />
-            </Button>
-          </AlertDialogTrigger>}
-          <DeleteProjectModal project={project} mutate={mutate} />
-        </AlertDialog>
-      </CardFooter>
-    </Card>
+
+      <Dialog open={openManageModal} onOpenChange={setOpenManageModal}>
+        <ManageBranchModal branches={project.branches} mutate={mutate} setOpenManageModal={setOpenManageModal} />
+      </Dialog>
+    </Card >
   );
 }
 
 export default ProjectCard2;
+
+
+// <CardFooter className="flex justify-between">
+//         
+//       </CardFooter>
