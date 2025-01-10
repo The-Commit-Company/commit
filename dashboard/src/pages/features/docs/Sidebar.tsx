cb8d32d70e81127db9670f03d2a1c8b41cf8d6c1
Sumit Jain <jainmit23@gmail.com>
2025-01-10 18:03:57 +0530

    refactor: enhance Sidebar component with route matching and prefetching logic

--- dashboard/src/pages/features/docs/Sidebar.tsx
+++ dashboard/src/pages/features/docs/Sidebar.tsx
@@ -2,12 +2,12 @@ import { CommitDocs } from "@/types/commit/CommitDocs";
 import { DocsSidebarItem } from "./docs";
 import DynamicIcon from "@/components/common/DynamicIconImport/IconComponent";
 import { Badge } from "@/components/ui/badge";
-import { useState } from "react";
-import classNames from "classnames";
+import { useCallback, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronRight } from "lucide-react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useGetCommitDocsDetails } from "@/components/features/meta_apps/useGetCommitDocsDetails";
+import { useFrappePrefetchCall } from "frappe-react-sdk";
 
 export const Sidebar = ({ ID }: { ID: string }) => {
 
@@ -70,9 +70,20 @@ const SidebarGroup = ({ groupName, items }: { groupName: string, items: DocsSide
 }
 
 const SidebarItem = ({ item, className, level = 1 }: { item: DocsSidebarItem, className?: string, level?: number }) => {
+    const { pageID } = useParams();
+
+    // Check if the current route is in the item's hierarchy
+    const isRouteMatch = (item: DocsSidebarItem): boolean => {
+        if (item.route === pageID) return true;
+        if (item.group_items) {
+            return item.group_items.some(isRouteMatch);
+        }
+        return false;
+    };
+
+    const [isExpanded, setIsExpanded] = useState(() => isRouteMatch(item));
 
     if (item.is_group_page && item.group_items?.length) {
-        const [isExpanded, setIsExpanded] = useState(false);
         return (
             <li>
                 <SidebarTitle item={item} className={className} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
@@ -94,64 +105,107 @@ const SidebarItem = ({ item, className, level = 1 }: { item: DocsSidebarItem, cl
     )
 }
 
-const SidebarTitle = ({ item, className, isExpanded, setIsExpanded }: { item: DocsSidebarItem, className?: string, isExpanded?: boolean, setIsExpanded?: (isExpanded: boolean) => void }) => {
+const BadgeClassMap = {
+    blue: 'bg-blue-500 hover:bg-blue-600',
+    red: 'bg-red-500 hover:bg-red-600',
+    green: 'bg-green-500 hover:bg-green-600',
+    yellow: 'bg-yellow-500 hover:bg-yellow-600',
+    purple: 'bg-purple-500 hover:bg-purple-600',
+    pink: 'bg-pink-500 hover:bg-pink-600',
+    indigo: 'bg-indigo-500 hover:bg-indigo-600',
+    cyan: 'bg-cyan-500 hover:bg-cyan-600',
+    teal: 'bg-teal-500 hover:bg-teal-600',
+    lime: 'bg-lime-500 hover:bg-lime-600',
+    orange: 'bg-orange-500 hover:bg-orange-600',
+    'blue-gray': 'bg-blue-gray-500 hover:bg-blue-gray-600',
+    gray: 'bg-gray-500 hover:bg-gray-600',
+    'true-gray': 'bg-true-gray-500 hover:bg-true-gray-600',
+    'warm-gray': 'bg-warm-gray-500 hover:bg-warm-gray-600',
+    'cool-gray': 'bg-cool-gray-500 hover:bg-cool-gray-600',
+}
 
-    const { pageID, ID } = useParams();
+const SidebarTitle = ({ item, className, isExpanded, setIsExpanded }: { item: DocsSidebarItem, className?: string, isExpanded?: boolean, setIsExpanded?: (isExpanded: boolean) => void }) => {
+    const { pageID } = useParams();
     const isSelected = item.route === pageID;
 
-    const badgeClass = classNames({
-        'text-[10px] px-1 py-0': true,
-        'bg-blue-500 hover:bg-blue-600': item.badge_color === 'blue',
-        'bg-red-500 hover:bg-red-600': item.badge_color === 'red',
-        'bg-green-500 hover:bg-green-600': item.badge_color === 'green',
-        'bg-yellow-500 hover:bg-yellow-600': item.badge_color === 'yellow',
-        'bg-purple-500 hover:bg-purple-600': item.badge_color === 'purple',
-        'bg-pink-500 hover:bg-pink-600': item.badge_color === 'pink',
-        'bg-indigo-500 hover:bg-indigo-600': item.badge_color === 'indigo',
-        'bg-cyan-500 hover:bg-cyan-600': item.badge_color === 'cyan',
-        'bg-teal-500 hover:bg-teal-600': item.badge_color === 'teal',
-        'bg-lime-500 hover:bg-lime-600': item.badge_color === 'lime',
-        'bg-orange-500 hover:bg-orange-600': item.badge_color === 'orange',
-        'bg-blue-gray-500 hover:bg-blue-gray-600': item.badge_color === 'blue-gray',
-        'bg-gray-500 hover:bg-gray-600': item.badge_color === 'gray',
-        'bg-true-gray-500 hover:bg-true-gray-600': item.badge_color === 'true-gray',
-        'bg-warm-gray-500 hover:bg-warm-gray-600': item.badge_color === 'warm-gray',
-        'bg-cool-gray-500 hover:bg-cool-gray-600': item.badge_color === 'cool-gray',
+    const BADGE_COLOR = BadgeClassMap[item.badge_color as keyof typeof BadgeClassMap ?? 'gray'];
+
+    const handleClick = () => {
+        if (item.is_group_page && setIsExpanded) {
+            setIsExpanded(!isExpanded);
+        }
+    };
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const preload = useFrappePrefetchCall('commit.commit.doctype.commit_docs_page.commit_docs_page.get_commit_docs_page', {
+        name: item.route
     })
 
-    return (
+    useEffect(() => {
+        let timeout: NodeJS.Timeout | null
+        if (isHovered) {
+            timeout = setTimeout(() => {
+                preload()
+            }, 250)
+        }
+
+        return () => {
+            if (timeout) {
+                clearTimeout(timeout)
+            }
+        }
+
+    }, [isHovered])
+
+    const onMouseEnter = useCallback(() => {
+        setIsHovered(true)
+    }, [])
+
+    const onMouseLeave = useCallback(() => {
+        setIsHovered(false)
+    }, [])
+
+    const content = (
+        <div className="flex justify-between items-center w-full ml-4 py-1" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+            <div className="flex items-center gap-2">
+                {item.icon && <DynamicIcon icon={item.icon} size={14} className={isSelected ? "text-blue-600" : "text-gray-500"} />}
+                {item.badge && <Badge className={cn('text-[10px] px-1 py-0', BADGE_COLOR)}>{item.badge}</Badge>}
+                <div className="">
+                    {item.title}
+                </div>
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+                {item.published === 0 && <Badge className="px-1 py-0 bg-orange-500 hover:bg-orange-600">Draft</Badge>}
+                {item.is_group_page && item.group_items?.length ? <span>{isExpanded ? <ChevronDown className={'h-4'} /> : <ChevronRight className={'h-4'} />}</span> : null}
+            </div>
+        </div>
+    );
+
+    return item.is_group_page ? (
         <div
             className={cn(
-                `group flex items-center py-1.5 cursor-pointer text-sm font-medium focus:outline-primary dark:focus:outline-primary-light space-x-3  border-l border-gray-200 dark:border-white/10 hover:border-gray-400 dark:hover:border-white/20 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300`,
+                `group flex items-center py-1.5 cursor-pointer text-sm font-medium focus:outline-primary dark:focus:outline-primary-light space-x-3 border-l border-gray-200 dark:border-white/10 hover:border-gray-400 dark:hover:border-white/20 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300`,
                 isSelected ? 'border-blue-600 dark:border-primary-light text-blue-600 dark:text-primary-light' : '',
                 className
             )}
-            onClick={item.is_group_page ? () => setIsExpanded && setIsExpanded(!isExpanded) : () => { }}
+            onClick={handleClick}
         >
-            <div className="flex justify-between items-center w-full ml-4 py-1">
-                {item.is_group_page ? <div className="flex items-center gap-2">
-                    {item.icon && <DynamicIcon icon={item.icon} size={14} className={isSelected ? "text-blue-600" : "text-gray-500"} />}
-                    {item.badge && <Badge
-                        className={cn(badgeClass, 'text-[10px] px-1 py-0')}>{item.badge}</Badge>}
-                    <div className="">
-                        {item.title}
-                    </div>
-                </div> : <a className="flex flex-row gap-2 w-full items-center" href={`../${ID}/${item.route}`}>
-                    {item.icon && <DynamicIcon icon={item.icon} size={14} className={isSelected ? "text-blue-600" : "text-gray-500"} />}
-                    {item.badge && <Badge
-                        className={cn(badgeClass, 'text-[10px] px-1 py-0')}>{item.badge}</Badge>}
-                    <div className="">
-                        {item.title}
-                        </div>
-                </a>}
-                <div className="flex flex-row gap-2 items-center">
-                    {item.published === 0 && <Badge className="px-1 py-0 bg-orange-500 hover:bg-orange-600">Draft</Badge>}
-                    {item.is_group_page && item.group_items?.length ? <span>{isExpanded ? <ChevronDown className={'h-4'} /> : <ChevronRight className={'h-4'} />}</span> : null}
-                </div>
-            </div>
+            {content}
         </div>
-    )
-}
+    ) : (
+        <Link
+            className={cn(
+                `group flex items-center py-1.5 cursor-pointer text-sm font-medium focus:outline-primary dark:focus:outline-primary-light space-x-3 border-l border-gray-200 dark:border-white/10 hover:border-gray-400 dark:hover:border-white/20 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300`,
+                isSelected ? 'border-blue-600 dark:border-primary-light text-blue-600 dark:text-primary-light' : '',
+                className
+            )}
+            to={`./${item.route}`}
+        >
+            {content}
+        </Link>
+    );
+};
 
 const SidebarSkeleton = () => {
     return (
