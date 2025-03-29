fc2b59b915edeb246181d9bbf8e8c7acbf4c0f3f
Sumit Jain <jainmit23@gmail.com>
2025-03-30 01:40:39 +0530

    feat: add animation to ViewDocs component for improved user experience

--- dashboard/src/pages/features/docs/ViewDocs.tsx
+++ dashboard/src/pages/features/docs/ViewDocs.tsx
@@ -1,10 +1,10 @@
 import { Outlet, useParams } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const ViewDocs = () => {
     const { ID } = useParams();
-
     if (ID) {
         return <ViewDocsDetails ID={ID} key={ID} />;
     }
@@ -13,19 +13,28 @@ const ViewDocs = () => {
 
 const ViewDocsDetails = ({ ID }: { ID: string }) => {
     const [isCollapsed, setIsCollapsed] = useState(false);
+
     return (
         <div className="relative antialiased">
             <div className="flex flex-col lg:flex-row min-h-screen">
+                {/* Sidebar */}
                 <Sidebar ID={ID} isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-                <div className="p-0 h-screen bg-zinc-100 dark:bg-white/[0.04] min-h-screen max-h-screen overflow-hidden w-full max-w-full min-w-full lg:p-2 lg:pl-0">
-                    <div className={`relative mt-[60px] max-lg:pb-[72px] lg:mt-0 lg:rounded-xl lg:border border-zinc-200/80 dark:border-zinc-800/80 bg-white dark:bg-zinc-950 min-h-full max-h-full h-full overflow-y-auto lg:px-10 pt-8 mb-6 ${isCollapsed ? 'lg:ml-14' : 'lg:ml-60'}`}>
-                        <div className="mx-auto max-w-6xl ">
+
+                {/* Animated Content */}
+                <motion.div
+                    initial={{ width: "calc(100% - 240px)", marginLeft: "15rem" }} // 15rem = 240px
+                    animate={{ width: isCollapsed ? "calc(100% - 56px)" : "calc(100% - 240px)", marginLeft: isCollapsed ? "3.5rem" : "15rem" }} // 3.5rem = 56px
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                    className="h-screen bg-zinc-100 dark:bg-white/[0.04] min-h-screen overflow-hidden flex-1 lg:p-2 lg:pl-0"
+                >
+                    <div className="relative mt-[60px] max-lg:pb-[72px] lg:mt-0 lg:rounded-xl lg:border border-zinc-200/80 dark:border-zinc-800/80 bg-white dark:bg-zinc-950 min-h-full max-h-full h-full overflow-y-auto lg:px-10 pt-8 mb-6">
+                        <div className="mx-auto max-w-6xl">
                             <main>
                                 <Outlet />
                             </main>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
